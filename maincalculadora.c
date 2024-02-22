2e88fc805041c2e03f52ebf4e4a8ceaecd3e99eb
Bicho jbo <118396444+JoseJimenez2004@users.noreply.github.com>
2024-02-21 21:12:44 -0600

    Create maincalculadora.c

---
+++ maincalculadora.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+//aqui poner cada quien sus cabeceras de funciones
+int main(){
+  int opc;
+  printf("Bienvenidos a la calculadora\n");
+  printf("Selecciona una opcion: \n");
+  printf("1. Suma\n");
+  printf("2. Resta\n");
+  printf("3. Multiplicación\n");
+  printf("4. División\n");
+  printf("5. Factorial\n");
+  printf("6. Raíz cuadrada\n");
+  scanf("%d", &opc);
+  switch(opc) {
+        case 1:
+          //logica de cada uno
+        break;
+        case 2:
+          //logica de cada uno
+        break;
+        case 3:
+          //logica de cada uno
+        break;
+        case 4:
+          //logica de cada uno
+        break;
+        case 5:
+          //logica de cada uno
+        break;
+        case 6:
+          //logica de cada uno
+        break;
+    default:
+    printf("OPCION INVALIDA\n");
+  }
+return 0;
+}
